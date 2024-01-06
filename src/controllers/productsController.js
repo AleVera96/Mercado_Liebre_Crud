9b32fd9709a434e36b54b61e02374a35875d2ba3
AleVera96 <alejandrovera.av96@gmail.com>
2024-01-06 15:26:10 -0300

    added multer library for upload images on product create

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -25,7 +25,15 @@ const controller = {
 	},
 	
 	// Create -  Method to store
-	store: (req, res) => {
+	store:(req,res,next)=>{
+			const files = req.files;
+			const filename = files.map(file => file.filename)
+			const json = fs.readFileSync(path.join(__dirname, "../data/productsDataBase.json"), "utf-8");
+			const products = JSON.parse(json);
+			  
+			if (!files || !files.length){
+		   return res.status(400).send('Por favor seleccione un archivo');
+			 }else{
 		const {name,price,discount,category,description} = req.body
 		const lastProduct = products[products.length - 1];
 		const newId = lastProduct ? lastProduct.id + 1 : 1;
@@ -35,13 +43,14 @@ const controller = {
 			price: price.trim(),
 			discount: discount.trim(),
 			category: category.trim(),
-			description: description.trim()
+			description: description.trim(),
+			image: filename,
 		}
 	
 		products.push(newProduct);
 		fs.writeFileSync(productsFilePath, JSON.stringify(products), 'utf-8');
 		 res.redirect('/products');
-	},
+	}},
 
 	// Update - Form to edit
 	edit: (req, res) => {
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,7 +1,19 @@
 // ************ Require's ************
 const express = require('express');
 const router = express.Router();
+const multer = require("multer")
 
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        const dataBase = path.join(__dirname, "../../public/images/products");
+      cb(null, dataBase)
+    },
+    filename:  (req, file, cb) =>{
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+  }
+  })
+   
+var fileUpload = multer({ storage: storage })
 // ************ Controller Require ************
 const productsController = require('../controllers/productsController');
 
@@ -10,7 +22,7 @@ router.get('/', productsController.index);
 
 /*** CREATE ONE PRODUCT ***/ 
 router.get('/create', productsController.create); 
-router.post('/create', productsController.store); 
+router.post('/create',fileUpload.array("image"), productsController.store); 
 
 
 /*** GET ONE PRODUCT ***/ 
